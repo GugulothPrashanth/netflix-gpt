610e1db8e7078cfd8dc894714013397e048ed027
Prashanth <prashanthg9849@gmail.com>
2025-12-06 22:54:21 +0530

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,7 +11,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+    Authorization: "Bearer " + import.meta.env.VITE_TMDB_KEY,
   },
 };
 
@@ -23,4 +23,4 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
-export const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_KEY;
+export const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_KEY;
